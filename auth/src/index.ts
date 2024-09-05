e7299b1574e164de489b0fadea88ee7f068a106f
Daniele Armando Sileoni <danielesileoni4@gmail.com>
2024-09-04 23:39:39 -0300

    add[jwt]

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 import { curretUserRouter } from "./routes/current-user";
 import { singinRouter } from "./routes/singin";
@@ -11,7 +12,14 @@ import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
+app.set("trust proxy", true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  }),
+);
 
 app.use(curretUserRouter);
 app.use(singinRouter);
@@ -25,6 +33,10 @@ app.all("*", async (req, res) => {
 app.use(errorHandler);
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+
   try {
     await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
     console.log("Success connect in data base");
--- auth/src/routes/singup.ts
+++ auth/src/routes/singup.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 import User from "../models/user";
 import { RequestValidationError } from "../errors/request-validation-error";
@@ -34,6 +35,18 @@ router.post(
       });
 
       await user.save();
+
+      const userJwt = jwt.sign(
+        {
+          id: user.id,
+          email: user.email,
+        },
+        process.env.JWT_KEY!,
+      );
+
+      req.session = {
+        jwt: userJwt,
+      };
 
       res.send(user);
     } catch (error) {
